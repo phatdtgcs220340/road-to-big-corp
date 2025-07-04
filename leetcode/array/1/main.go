d26b0781fdd4a892225fa48b0bb40da777d10460
Đỗ Tấn Phát <phatdtgcs220340@fpt.edu.vn>
2025-07-04 21:46:38 +0700

    two sum with go

---
+++ leetcode/array/1/main.go
@@ -0,0 +1,25 @@
+package main 
+
+import "fmt"
+func main() {
+	nums := []int{2, 7, 11, 15}
+	result := twoSum(nums, 9)
+
+	for _, n := range result {
+		fmt.Printf("%d ", n)
+	}
+}
+
+func twoSum(nums []int, target int) []int {
+	pos := make(map[int]int)
+	for i, n := range nums {
+		value, exists := pos[target - n]
+		if exists  {
+			return []int{value, i}
+		} else {
+			pos[n] = i
+		}
+	}
+	return nil
+}
+
